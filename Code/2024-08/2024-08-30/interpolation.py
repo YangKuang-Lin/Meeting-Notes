8bf079aca2a0714659479d93a35e1c708a9bcb87
YK Lin <mamiya.nonoka@gmail.com>
2024-08-12 21:45:11 +0900

    Create interpolation.py

---
+++ Code/2024-08/2024-08-30/interpolation.py
@@ -0,0 +1,31 @@
+def interpotation(x1, x2, l, t):
+    """
+    The function returns the interpolated image.
+
+    Parameters
+    ----------
+    x1 : tensor
+        Image x0.
+    x2 : tensor
+        Image x'0
+    l : float
+        Lambda.
+    t : float
+        Time.
+
+    Returns
+    -------
+    tensor
+        The interpolated image.
+
+    Notes
+    -----
+    .. [1] source, https://nn.labml.ai/diffusion/ddpm/evaluate.html
+
+    """
+
+    # diffusion.q denotes the non-parametric forward process to
+    # add noise into the image for timestep t.
+    x = (1 - l) * diffusion.q(x1, t) + l * diffusion.q(x2, t)
+
+    return x
